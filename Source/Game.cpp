295b38f4c5e45f4037449f681d33bed69a48ed33
Cristian <cris_am07@hotmail.com>
2014-09-16 19:56:36 -0700

    Actividad 2
    Agregando motor de estados

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -19,11 +19,15 @@ bool CGame::Start()
             
 		//Maquina de estados
 		switch(estado){
-			case 0: //INICIALIZAR
+		case Estado::ESTADO_INICIANDO:
 			break;
-			case 1:	//JUGAR	
+		case Estado::ESTADO_MENU:
 			break;
-			case 2: //SALIR
+		case Estado::ESTADO_JUGANDO:
+			break;
+		case Estado::ESTADO_TERMINANDO:
+			break;
+		case Estado::ESTADO_FINALIZANDO:
 				salirJuego = true;
 			break;
 		};
