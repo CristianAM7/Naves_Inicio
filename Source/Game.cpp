a88aa7fea1f23153346b98ac27a4ab1b3195e2c3
Vladimir <viperezf@upbc.edu.mx>
2014-09-22 10:49:20 -0700

    Actividad 2: Mal

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -3,7 +3,7 @@
 #include "Game.h"
 
 CGame::CGame(){
-	Estado estado;////ACT2 <-- Ahora aca tienes un error
+	Estado estado;////ACT2: Mal, si te fijas en "Game.h" ya tienes esta variable dada de alta, ahora debes de darle el valor inicial, indica cual es tu primer estado.
 }
 
 // Con esta función eliminaremos todos los elementos en pantalla
